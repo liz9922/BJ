e8b349c0be396d6666988ed083fee4f8b77d5b51
liz9922 <124145079+liz9922@users.noreply.github.com>
2023-06-03 22:56:30 +0200

    Initial commit

---
+++ src/sqlite/SQLite.java
@@ -0,0 +1,77 @@
+import java.sql.*;
+public class SQLite {
+    private Connection connection;
+
+    public SQLite (){
+        connect("Lizzie");
+
+
+    }
+    // funkar
+    public void connect(String databasePath) {
+        try {
+            // Anslut till databasen
+            connection = DriverManager.getConnection("jdbc:sqlite:" + databasePath + ".db");
+            System.out.println("Databasanslutning etablerad.");
+        } catch (SQLException e) {
+            System.err.println("Anslutningsfel: " + e.getMessage());
+        }
+    }
+
+    // funkar
+    public void disconnect() {
+        try {
+            // Stänger anslutningen till databasen
+            if (connection != null) {
+                connection.close();
+                System.out.println("Databasanslutning stängd.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Stängningsfel: " + e.getMessage());
+        }
+    }
+    public void createTablePlayer(){
+
+        String SQL = "CREATE TABLE IF NOT EXISTS Player("
+                + "Player_id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                +"Player_name VARCHAR(30) NOT NULL, \n"
+                +"Amount_killed INTEGER, \n"
+                +"Amount_captured INTEGER);";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            statement.execute();
+            System.out.println("Tabell 'player' skapad.");
+        } catch (SQLException e) {
+            System.err.println("Fel med att skapa tabell 'player': " + e.getMessage());
+        }
+
+    }
+    public void createTerroristTable(){
+        String SQL = "CREATE TABLE IF NOT EXISTS Terrorist("
+                + "Terrorist_id INTEGER PRIMARY KEYAUTOINCREMENT,\n"
+                + "Terrorist_name VARCHAR(100)NOT NULL,\n"
+                + "Terrorist_pic_url VARCHAR (200),\n"
+                +"Terrorist_description TEXT,\n"
+                +"Alive VARCHAR(1),"
+                +"";
+    }
+    public Integer security(Terrorist terrorist) {
+        String sql = "SELECT terrorist_id FROM Terrorist "
+                + "WHERE Terrorist_name=? AND pic=?";
+        try {
+            PreparedStatement p = connection.prepareStatement(sql);
+            p.setString(1, terrorist.getName);
+            p.setString(2, terrorist.getPic());
+            ResultSet r = p.executeQuery();
+            if (r.next()) {
+                return r.getInt("Terrorist_id");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage() + " fel med att hämta användare");
+        } return null;
+
+    }
+
+
+}
